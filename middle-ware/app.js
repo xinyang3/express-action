3c0d3f90b8047a01e31ac8451e257580bf12002c
xinyang3 <xinyang.zhu@outlook.com>
2020-03-31 14:36:50 +0800

    中间件错误拦截

--- middle-ware/app.js
+++ middle-ware/app.js
@@ -12,10 +12,35 @@ app.use(morgan("short"));
 var staticPath = path.join(__dirname, "static");
 app.use(express.static(staticPath));
 
+// 错误拦截处理
+app.use((req, res, next) => {
+    if(req.url === '/1.png') {
+        var filePath = path.resolve(__dirname, 'static', '1.png');
+        res.sendFile(filePath, function (err) {
+            if (err) {
+                next(new Error("Error sending file!"));
+            }
+        });
+    } else {
+        next()
+    }
+});
+
 app.use(function (req, res) {
     res.status(404);
     res.send("File not found!");
 });
+
+app.use(function (err, req, res, next) {
+    // console.error(err);
+    next(err);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(500);
+    res.send("Internal server error.");
+});
+
 app.listen(3000, function () {
     console.log("App started on port 3000");
 });
